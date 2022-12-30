df48a14046290c4f8a749a4ca963e0115efc23cc
minimAluminiumalism <25639317+minimAluminiumalism@users.noreply.github.com>
2022-12-30 23:58:51 +0800

    new function added: config header

--- header.go
+++ header.go
@@ -7,11 +7,11 @@ import (
 
 func headerConfig(session *Session) {
 	configPath := "./headers.yaml"
-	_, err := proxyutil.ParseConfig(configPath)
+	header, err := proxyutil.ParseConfig(configPath)
 	if err != nil {
 		log.Error("Header configuration error, ", err)
 	}
-	// userAgent := 
-	// cookie := 
-	session.req.Header = map[string][]string{"cookie": {"csrftoken=YisZfC5ccRSFnSWlUfd2fp8l7S25gKtu26EggpuyqUtC3v5pSy54baeZNzvstcUX; sessionid=lmp90aiknolao6yaluugtp7stdlg4cgm"}}
+	session.req.Header = make(map[string][]string)
+	session.req.Header["user-agent"] = []string{header.UserAgent}
+	session.req.Header["cookie"] = []string{header.Cookie}
 }
--- proxyutil/config_test.go
+++ proxyutil/config_test.go
@@ -1,14 +1,17 @@
 package proxyutil
 
 import (
+	"fmt"
 	"testing"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestParseConfig(t *testing.T) {
 	t.Run("config file", func (t *testing.T) {
 		configPath := "../headers.yaml"
-		_, err := ParseConfig(configPath)
+		header, err := ParseConfig(configPath)
 		require.Nil(t, err)
+		fmt.Println(header.UserAgent)
 	})
 }
